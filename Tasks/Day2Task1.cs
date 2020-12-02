2378320d7532e245ba199b24270f0d3a7e2ecc40
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-12-03 00:20:53 +0100

    Solved day 2

---
+++ Tasks/Day2Task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day2Task1
+    {
+        public static string Execute(string input)
+        {
+            var lines = input.Split("\n")
+                .Select(line =>
+                    new Line(
+                        Min: int.Parse(line.Split('-')[0]),
+                        Max: int.Parse(line.Split(' ')[0].Split('-')[1]),
+                        Character: line.Split(':')[0].Last(),
+                        Password: line.Split(' ')[2]
+                    )
+                ).ToList();
+            return lines.Count(line => Enumerable.Range(line.Min, line.Max - line.Min + 1).Contains(line.Password.Count(c => c == line.Character))).ToString();
+        }
+
+        public record Line(int Min, int Max, char Character, string Password);
+
+    }
+}
---
+++ Tasks/Day2Task2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day2Task2
+    {
+        public static string Execute(string input)
+        {
+            var lines = input.Split("\n")
+                .Select(line =>
+                    new Line(
+                        Pos1: int.Parse(line.Split('-')[0])-1,
+                        Pos2: int.Parse(line.Split(' ')[0].Split('-')[1])-1,
+                        Character: line.Split(':')[0].Last(),
+                        Password: line.Split(' ')[2]
+                    )
+                ).ToList();
+            return lines.Count(line => line.Password[line.Pos1] == line.Character ^ line.Password[line.Pos2] == line.Character).ToString();
+        }
+
+        public record Line(int Pos1, int Pos2, char Character, string Password);
+
+    }
+}
