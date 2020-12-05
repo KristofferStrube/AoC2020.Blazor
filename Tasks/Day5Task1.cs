9e63e2ba3226267c23803a3b990de66893040db0
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-12-05 20:43:11 +0100

    Done with day5

---
+++ Tasks/Day5Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day5Task1
+    {
+        public static string Execute(string input)
+        {
+            return input.Replace("\r", "").Split("\n").Select(line =>
+            line.Substring(0, 7).Select((c, i) => c == 'B' ? Math.Pow(2, 7 - i - 1) : 0).Sum()*8+
+            line.Substring(7, 3).Select((c, i) => c == 'R' ? Math.Pow(2, 3 - i - 1) : 0).Sum()
+            ).Max().ToString();
+        }
+    }
+}
---
+++ Tasks/Day5Task2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day5Task2
+    {
+        public static string Execute(string input)
+        {
+            var IDs = input.Replace("\r", "").Split("\n").Select(line =>
+            line.Substring(0, 7).Select((c, i) => c == 'B' ? Math.Pow(2, 7 - i - 1) : 0).Sum() * 8 +
+            line.Substring(7, 3).Select((c, i) => c == 'R' ? Math.Pow(2, 3 - i - 1) : 0).Sum()
+            );
+            return (IDs.First(id1 => !IDs.Contains(id1+1) && IDs.Any(id2 => id2 - id1 == 2)) + 1).ToString();
+        }
+    }
+}
