5bb05a45c0f35ed6df1411344a72e5b1371f09f3
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-12-06 06:19:46 +0100

    Done with day6

---
+++ Tasks/Day6Task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day6Task1
+    {
+        public static string Execute(string input)
+        {
+            return input.Replace("\r", "").Split("\n\n").Select(g => g.Replace("\n", "").Distinct().Count()).Sum().ToString();
+        }
+    }
+}
---
+++ Tasks/Day6Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day6Task2
+    {
+        public static string Execute(string input)
+        {
+            return input.Replace("\r", "").Split("\n\n").Select(g => g.Split("\n").Skip(1).Aggregate((IEnumerable<char>)g.Split("\n").First(), (inter, p) => inter.Intersect(p)).Count()).Sum().ToString();
+        }
+    }
+}
