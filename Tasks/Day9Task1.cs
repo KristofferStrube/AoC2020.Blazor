43d2f8222790ba72d05e0192ca2158bdbe17804e
KristofferStrube <kristoffer.strube@gmail.com>
2020-12-12 15:52:06 +0100

    Done with day9

---
+++ Tasks/Day9Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day9Task1
+    {
+        public static string Execute(string preambleSize, string input)
+        {
+            var preamble_size = int.Parse(preambleSize);
+            var lines = input.Replace("\r", "").Split("\n").Select(line => long.Parse(line)).ToList();
+            return lines[Enumerable.Range(preamble_size, lines.Count()-preamble_size).First(x => !lines.GetRange(x- preamble_size, preamble_size).Any(y => lines.GetRange(x - preamble_size, preamble_size).Any(z => y + z == lines[x])))].ToString();
+        }
+    }
+
+}
---
+++ Tasks/Day9Task2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day9Task2
+    {
+        public static string Execute(string preambleSize, string input)
+        {
+            var lines = input.Replace("\r", "").Split("\n").Select(line => long.Parse(line)).ToList();
+            var invalidNumber = int.Parse(Day9Task1.Execute(preambleSize, input));
+            var ranges = Enumerable.Range(0, lines.Count()).Select(x => Enumerable.Range(0, lines.Count() - x).Select(offset => new Tuple<int, int>(x, offset))).SelectMany(r => r);
+            var correctRange = ranges.First(r => lines.GetRange(r.Item1, r.Item2).Sum() == invalidNumber);
+            var smallest = lines.GetRange(correctRange.Item1, correctRange.Item2).Min();
+            var largest = lines.GetRange(correctRange.Item1, correctRange.Item2).Max();
+            return (smallest + largest).ToString();
+        }
+    }
+
+}
