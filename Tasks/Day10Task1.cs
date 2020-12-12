b35335f9835aabd93737a755cd02038f6f4ecbb8
KristofferStrube <kristoffer.strube@gmail.com>
2020-12-12 18:01:41 +0100

    Done with day10

---
+++ Tasks/Day10Task1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day10Task1
+    {
+        public static string Execute(string input)
+        {
+            var lines = input.Replace("\r", "").Split("\n").Select(line => int.Parse(line)).ToList();
+            lines.Sort();
+            var ones = Enumerable.Range(0, lines.Count() - 1).Count(x => lines[x + 1] - lines[x] == 1)+1;
+            var threes = Enumerable.Range(0, lines.Count() - 1).Count(x => lines[x + 1] - lines[x] == 3)+1;
+            return (ones * threes).ToString();
+        }
+    }
+
+}
---
+++ Tasks/Day10Task2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day10Task2
+    {
+        private static Dictionary<int, long> subtree = new();
+
+        private static List<long> lines = new();
+
+        private static long deviceJolt;
+
+        public static string Execute(string input)
+        {
+            lines = input.Replace("\r", "").Split("\n").Select(line => long.Parse(line)).ToList();
+            lines.Add(0);
+            lines.Sort();
+            deviceJolt = lines.Last() + 3;
+            return calcPerms(0).ToString();
+        }
+
+        private static long calcPerms(int index)
+        {
+            if (index == lines.Count())
+            {
+                return 1;
+            }
+            else if (subtree.ContainsKey(index))
+            {
+                return subtree[index];
+            }
+            else
+            {
+                var result = Enumerable.Range(index + 1, 3)
+                        .Where(i => (i < lines.Count() && lines[i] - lines[index] <= 3)
+                        || (i == lines.Count() && deviceJolt- lines[index] <= 3))
+                        .Select(i => calcPerms(i))
+                        .Sum();
+                subtree[index] = result;
+                return result;
+            }
+        }
+    }
+
+}
