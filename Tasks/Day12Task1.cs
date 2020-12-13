bc6e8207111ad6f5046b70af56b3a65fcaeee170
KristofferStrube <kristoffer.strube@gmail.com>
2020-12-13 02:35:19 +0100

    Done with day12

---
+++ Tasks/Day12Task1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day12Task1
+    {
+        public static string Execute(string input)
+        {
+            var lines = input.Replace("\r", "").Split("\n");
+            var endState = lines.Aggregate((orientation: 90, x: 0, y: 0), (state, comm) =>
+            {
+                switch (comm[0])
+                {
+                    case 'N': state.x += int.Parse(comm[1..]); break;
+                    case 'S': state.x -= int.Parse(comm[1..]); break;
+                    case 'E': state.y += int.Parse(comm[1..]); break;
+                    case 'W': state.y -= int.Parse(comm[1..]); break;
+                    case 'L': state.orientation = (360 + state.orientation - int.Parse(comm[1..])) % 360; break;
+                    case 'R': state.orientation = (state.orientation + int.Parse(comm[1..])) % 360; break;
+                    case 'F':
+                        switch (state.orientation)
+                        {
+                            case 0: state.x += int.Parse(comm[1..]); break;
+                            case 90: state.y += int.Parse(comm[1..]); break;
+                            case 180: state.x -= int.Parse(comm[1..]); break;
+                            case 270: state.y -= int.Parse(comm[1..]); break;
+                        }; break;
+                }
+                return state;
+            });
+            return (Math.Abs(endState.x) + Math.Abs(endState.y)).ToString();
+        }
+    }
+
+}
---
+++ Tasks/Day12Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020.Blazor.Tasks
+{
+    public static class Day12Task2
+    {
+        public static string Execute(string input)
+        {
+            var lines = input.Replace("\r", "").Split("\n");
+            var endState = lines.Aggregate((x: 0, y: 0, wx: 1, wy: 10), (state, comm) =>
+            {
+                var letter = comm[0];
+                var numb = int.Parse(comm[1..]);
+                switch (letter)
+                {
+                    case 'N': state.wx += numb; break;
+                    case 'S': state.wx -= numb; break;
+                    case 'E': state.wy += numb; break;
+                    case 'W': state.wy -= numb; break;
+                    case 'L':
+                        switch (numb)
+                        {
+                            case 90: (state.wx, state.wy ) = (state.wy, -state.wx); break;
+                            case 180: state.wx *= -1; state.wy *= -1; break;
+                            case 270: (state.wx, state.wy) = (-state.wy, state.wx); break;
+                        }
+                        break;
+                    case 'R':
+                        switch (numb)
+                        {
+                            case 90: (state.wx, state.wy) = (-state.wy, state.wx); break;
+                            case 180: state.wx *= -1; state.wy *= -1; break;
+                            case 270: (state.wx, state.wy) = (state.wy, -state.wx); break;
+                        }
+                        break;
+                    case 'F': state.x += numb * state.wx; state.y += numb * state.wy; break;
+                }
+                return state;
+            });
+            return (Math.Abs(endState.x) + Math.Abs(endState.y)).ToString();
+        }
+    }
+
+}
